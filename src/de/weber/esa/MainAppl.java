190a125f834b82a9ce4d0db2aca70a4738d53615
weberstefan <weberste@cip.ifi.lmu.de>
2017-03-13 11:02:57 +0100

    Updates

--- src/de/weber/esa/MainAppl.java
+++ src/de/weber/esa/MainAppl.java
@@ -1,8 +1,6 @@
 package de.weber.esa;
 
 import de.weber.esa.struct.EnhancedSuffixArray;
-import de.weber.esa.struct.repeats.MaximalRepeats;
-import de.weber.esa.struct.repeats.SupermaximalRepeats;
 import de.weber.esa.struct.rmq.RMQ;
 import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 
@@ -40,21 +38,21 @@ public class MainAppl {
 //                "ESTHEBREAKDOWNOFMANUFACTURESANDTRANSPORTATIONINALLTHESEWEKNOWNOWWEREPARTOFAGIGANTICCAMPA" +
 //                "IGNOFSABOTAGETHISWASHALTEDJUSTINTIMEBYTHEMARCHREVOLUTION";
 //        final String testQuery = "AA";
-//
+
 //        FMIndex fm = esa.bwt.backwardSearch(esa, testQuery);
 //        System.out.println(testQuery + ": " + fm.toString());
 //        System.out.println("FM-backward search done: " + Calendar.getInstance().getTime());
-//
-        SupermaximalRepeats sr = new SupermaximalRepeats(esa);
-        System.out.println("Supermaximal repeats: " + sr.getSupermaximalRepeats());
-        System.out.println("Supermaximal Repeats built: " + Calendar.getInstance().getTime());
-//
-        MaximalRepeats mr = new MaximalRepeats(esa);
-        System.out.println("Maximal Repeats: " + mr.getMaximalRepeats());
-        System.out.println("Maximal Repeats built: " + Calendar.getInstance().getTime());
-//
+
+//        SupermaximalRepeats sr = new SupermaximalRepeats(esa);
+//        System.out.println("Supermaximal repeats: " + sr.getSupermaximalRepeats());
+//        System.out.println("Supermaximal Repeats built: " + Calendar.getInstance().getTime());
+
+//        MaximalRepeats mr = new MaximalRepeats(esa);
+//        System.out.println("Maximal Repeats: " + mr.getMaximalRepeats());
+//        System.out.println("Maximal Repeats built: " + Calendar.getInstance().getTime());
+
         RMQ rmq = new RMQ(esa.lcp.lcps, 3, true);
-        System.out.println(rmq.toString());
+//        System.out.println(rmq.toString());
 
 
 //        System.out.println(esa.toString());
