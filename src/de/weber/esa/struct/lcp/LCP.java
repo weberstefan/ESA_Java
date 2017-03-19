ac8369b944f545ef7877f392d04431457aa07c9e
weberstefan <weberste@cip.ifi.lmu.de>
2017-03-19 10:54:25 +0100

    Update LCP and discriminating characters

--- src/de/weber/esa/struct/lcp/LCP.java
+++ src/de/weber/esa/struct/lcp/LCP.java
@@ -66,9 +66,11 @@ public class LCP {
 
             /*
                 TODO Space efficiently compute discriminating characters
-
-                this.discriminatingCharactersMap.put(i, new DiscriminatingCharacters(esa.sequence[b + k], esa.sequence[i + k]));
             */
+
+            final DiscriminatingCharacters dc = new DiscriminatingCharacters(esa.sequence[b + k], esa.sequence[i + k]);
+            this.discriminatingCharactersMap.put(a, dc);
+
 
             k = Math.max(0, k - 1);
         }
